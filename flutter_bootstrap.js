2587645bb536d249604a4d110660884259f36d0f
jameel <android2@theinteractive.in>
2024-09-29 18:51:32 +0530

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3113929396"
+    serviceWorkerVersion: "1499592501"
   }
 });
